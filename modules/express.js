2d70e3590991f5502ee6a1d454fabefeb92ab520
Francisco Alves <fcoocf@hotmail.com>
2022-06-02 18:45:24 +0000

    Implementando o front

--- modules/express.js
+++ modules/express.js
@@ -4,6 +4,22 @@ const app = express();
 
 app.use(express.json());
 
+app.set('view engine', 'ejs');
+app.set('views', 'src/views');
+
+app.use((req, res, next) => {
+    console.log(`Request type : ${req.method}`);
+    console.log(`Content type : ${req.headers["content-type"]}`);
+    console.log(`Date : ${new Date()}`);
+
+    next();
+});
+
+app.get('/views/users', async (req, res) => {
+    const users = await UserModel.find({});
+    res.render('index', { users });
+});
+
 app.get("/users", async (req, res) => {
 
     try {
@@ -14,10 +30,43 @@ app.get("/users", async (req, res) => {
     }
 });
 
+app.get("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 app.post("/users", async (req, res) => {
     try {
         const user = await UserModel.create(req.body);
         res.status(201).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.patch("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {new: true});
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.delete("/users/:id", async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndRemove(id);
+        res.status(200).json(user);
     } catch (error) {
         res.status(500).send(error.message);
     }
